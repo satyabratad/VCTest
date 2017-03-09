ac7d1942a4b38f924e3fb622bc106796e6e0db7e
JoyG <JoyG@Bill2PayTest>
2017-03-09 05:02:14 -0500

    ﻿Batch .pdf generation [git-vault-id] RS Software$@220/53350

--- 1099K 1.1/Bill2Pay.Web/Controllers/PrintController.cs
+++ 1099K 1.1/Bill2Pay.Web/Controllers/PrintController.cs
@@ -8,6 +8,7 @@ using System.Web.Mvc;
 using System.IO;
 using Bill2Pay.GenerateIRSFile;
 using System.Web.Hosting;
+using Bill2Pay.ExceptionLogger;
 
 namespace Bill2Pay.Web.Controllers
 {
@@ -47,12 +48,12 @@ namespace Bill2Pay.Web.Controllers
         public ActionResult PrintAllCopies()
         {
             HostingEnvironment.QueueBackgroundWorkItem(clt => PrintCopies());
-            //PrintCopies();
+            TempData["successMessage"] = "Generate .pdf file process may take some time. Once completed you can find the files in the specified location. A transaction log is also available. ";
             return RedirectToAction("Index", "IRSProcess");
         }
         public void PrintCopies()
         {
-            //var rootpath = Server.MapPath(string.Format("~/App_Data/Download/k1099/{0}/", DateTime.Now.Ticks));
+            var errorAccounts = string.Empty;
             var rootpath = Server.MapPath(string.Format("~/App_Data/Download/k1099/"));
             System.IO.Directory.CreateDirectory(rootpath);
 
@@ -66,25 +67,6 @@ namespace Bill2Pay.Web.Controllers
 
                 folderName = accno + "-" + year.ToString();
 
-
-
-                // MerchantListVM detail = ApplicationDbContext.Instence.ImportDetails //ApplicationDbContext.Instence.ImportDetails
-                //.Include("Merchant")
-                //.GroupJoin(ApplicationDbContext.Instence.SubmissionStatus.Where(s => s.IsActive == true),
-                //    imp => imp.AccountNo,
-                //    stat => stat.AccountNumber,
-                //    (imp, stat) => new MerchantListVM() { ImportDetails = imp, SubmissionStatus = stat.FirstOrDefault() })
-                //.OrderByDescending(p => p.ImportDetails.ImportSummaryId)
-                //.FirstOrDefault(p => p.ImportDetails.AccountNo.Equals(Id, StringComparison.OrdinalIgnoreCase) && p.ImportDetails.IsActive == true);
-
-
-                // var merchantData = (ImportDetail)detail.ImportDetails;
-                // if (detail.SubmissionStatus == null || detail.SubmissionStatus.Status.Id <= 2)
-                //     merchantData.SubmissionSummaryId = null;
-
-                // var merchant = ApplicationDbContext.Instence.MerchantDetails.Where(m => m.Id == data.MerchantId).FirstOrDefault();
-                // merchantData.Merchant = merchant;
-
                 var merchantData = ApplicationDbContext.Instence.SubmissionDetails
                   .Include("PSE")
                   .OrderByDescending(p => p.SubmissionId)
@@ -94,81 +76,99 @@ namespace Bill2Pay.Web.Controllers
                 if (merchantData != null)
                 {
                     data.Add(merchantData);
-                }
-
-                var pseData = new List<PSEDetails>();
-                if (merchantData.PSE != null)
-                {
-                    pseData.Add(merchantData.PSE);
-                }
-                int TransactionYear = merchantData.SubmissionSummary.PaymentYear + 1;
-                string[] reportNames = { "CopyA", "Copy1", "CopyB", "Copy2", "CopyC" };
-                LocalReport localReport;
-                //foreach (var reportName in reportNames)
-                //{
-                     var reportName = "CopyA";
-                    //yield return DetailsReport(reportName, accno);merchantData
-                    localReport = new LocalReport();
-                    localReport.ReportPath = Server.MapPath("~/Reports/" + reportName + ".rdlc");
-                    ReportDataSource reportDataSource = new ReportDataSource("SubmissionDetails", data);
-                    
-
-                    var yearParam = new ReportParameter("TransactionYear", TransactionYear.ToString());
-                    localReport.SetParameters(yearParam);
-                    localReport.DataSources.Add(reportDataSource);
-
-                    ReportDataSource pseDataSource = new ReportDataSource("PSEMaster", pseData);
-
-                    localReport.DataSources.Add(pseDataSource);
-
-                    string reportType = "PDF";
-                    string mimeType;
-                    string encoding;
-                    string fileNameExtension;
-
-                    //The DeviceInfo settings should be changed based on the reportType
-                    //http://msdn.microsoft.com/en-us/library/ms155397.aspx
-                    string deviceInfo =
-                    "<DeviceInfo>" +
-                    "  <OutputFormat>PDF</OutputFormat>" +
-                    "  <PageWidth>8.75in</PageWidth>" +
-                    "  <PageHeight>11in</PageHeight>" +
-                    "  <MarginTop>0.5in</MarginTop>" +
-                    "  <MarginLeft>0.5in</MarginLeft>" +
-                    "  <MarginRight>0.5in</MarginRight>" +
-                    "  <MarginBottom>0.5in</MarginBottom>" +
-                    "</DeviceInfo>";
-
-                    Warning[] warnings;
-                    string[] streams;
-                    byte[] renderedBytes;
-
-                    //Render the report
-                    renderedBytes = localReport.Render(
-                        reportType,
-                        deviceInfo,
-                        out mimeType,
-                        out encoding,
-                        out fileNameExtension,
-                        out streams,
-                        out warnings);
 
 
-                    var path = string.Format("{0}/{1}/", rootpath, folderName);
-                    if (!System.IO.Directory.Exists(path))
+                    var pseData = new List<PSEDetails>();
+                    if (merchantData.PSE != null)
                     {
-                        System.IO.Directory.CreateDirectory(path);
+                        pseData.Add(merchantData.PSE);
                     }
+                    int TransactionYear = merchantData.SubmissionSummary.PaymentYear + 1;
+                    string[] reportNames = { "CopyA", "Copy1", "CopyB", "Copy2", "CopyC" };
+                    LocalReport localReport;
+                    foreach (var reportName in reportNames)
+                    {
+                        //var reportName = "CopyA";
 
-                    path = string.Format("{0}/{1}/{2}.pdf", rootpath, folderName, reportName);
-                    //File(renderedBytes, mimeType);
-                    System.IO.File.WriteAllBytes(path, renderedBytes); // Requires System.IO
-                                                                       //Response.AddHeader("content-disposition", "attachment; filename=NorthWindCustomers." + fileNameExtension);
-                //}
+                        localReport = new LocalReport();
+                        localReport.ReportPath = Server.MapPath("~/Reports/" + reportName + ".rdlc");
+                        ReportDataSource reportDataSource = new ReportDataSource("SubmissionDetails", data);
+
+
+                        var yearParam = new ReportParameter("TransactionYear", TransactionYear.ToString());
+                        localReport.SetParameters(yearParam);
+                        localReport.DataSources.Add(reportDataSource);
+
+                        ReportDataSource pseDataSource = new ReportDataSource("PSEMaster", pseData);
+
+                        localReport.DataSources.Add(pseDataSource);
+
+                        string reportType = "PDF";
+                        string mimeType;
+                        string encoding;
+                        string fileNameExtension;
+
+                        //The DeviceInfo settings should be changed based on the reportType
+                        //http://msdn.microsoft.com/en-us/library/ms155397.aspx
+                        string deviceInfo =
+                        "<DeviceInfo>" +
+                        "  <OutputFormat>PDF</OutputFormat>" +
+                        "  <PageWidth>8.75in</PageWidth>" +
+                        "  <PageHeight>11in</PageHeight>" +
+                        "  <MarginTop>0.5in</MarginTop>" +
+                        "  <MarginLeft>0.5in</MarginLeft>" +
+                        "  <MarginRight>0.5in</MarginRight>" +
+                        "  <MarginBottom>0.5in</MarginBottom>" +
+                        "</DeviceInfo>";
+
+                        Warning[] warnings;
+                        string[] streams;
+                        byte[] renderedBytes;
+
+                        //Render the report
+                        renderedBytes = localReport.Render(
+                            reportType,
+                            deviceInfo,
+                            out mimeType,
+                            out encoding,
+                            out fileNameExtension,
+                            out streams,
+                            out warnings);
+
+
+                        var path = string.Format("{0}/{1}/", rootpath, folderName);
+                        if (!System.IO.Directory.Exists(path))
+                        {
+                            System.IO.Directory.CreateDirectory(path);
+                        }
+
+                        path = string.Format("{0}/{1}/{2}.pdf", rootpath, folderName, reportName);
+                        //File(renderedBytes, mimeType);
+                        System.IO.File.WriteAllBytes(path, renderedBytes); // Requires System.IO
+                                                                           //Response.AddHeader("content-disposition", "attachment; filename=NorthWindCustomers." + fileNameExtension);
+
+                        
+                    }
+                    Logger.LogInstance.LogInfo("Pdf file(s) generated for :{0} and the same can be found {1}", accno, folderName);
+                }
+                else
+                {
+                    errorAccounts = errorAccounts + ", " + accno ;
+                    Logger.LogInstance.LogInfo("System unable to generate .pdf file(s) for :{0} ", accno);
+
+                }
+                
+                
 
                 //ImportUtility.CreateZip(rootpath);
             }
 
+            //TempData["successMessage"] = ".pdf file can be found in " + rootpath + " folder. For details see transaction log ";
+            //if (errorAccounts.Length > 2)
+            //{
+            //    errorAccounts = errorAccounts.Substring( errorAccounts.Length - 2);
+            //    TempData["errorMessage"] = "System unable to generate .pdf file(s) for :" + errorAccounts + ". For details see transaction log ";
+            //}
         }
 
         public ActionResult DetailsReport(string reportName, string Id)
