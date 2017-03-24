7861bf7ab668f8b42bc067c5f80a32150de7b1d1
ArghyaB <ArghyaB@Bill2PayTest>
2017-03-24 11:37:57 -0400

    ﻿ [git-vault-id] RS Software$@343/63575

---
+++ TaxCollector/Html Prototype/SSO/content/scripts/custom/PaymentDetails.js
@@ -0,0 +1,150 @@
+﻿// JavaScript source code
+
+var bill2payPaymentDetails = {
+showCart: function(){
+	debugger;
+	$("#clientName").html(dbObject.CustomerName);
+	var param=getParameterByName('dbObject');
+	DeSrializeDbObject(param);
+	populateBreadcrumb();
+	$("#cartCount").html((dbObject.Products.length==0?"":dbObject.Products.length));
+	bill2payPaymentDetails.populateSubtotal();
+	bill2payPaymentDetails.populateOrderDetails();
+	bill2payPaymentDetails.populateCreditCardDetails();
+},
+redirectToCartGrid: function(){
+	var json=SerializeDbObject();		
+      redirect('Homelookup.html?ShowCart=Y&dbObject=' + json);	
+},
+redirectToPaymentConfirm: function(){
+	debugger;
+	var firstTab = $('a[data-toggle="tab"]:first');
+            var tabName = null;
+			var paymentType=null;
+            // Get the last active tab before postback
+            tabName = $("[id*=hdfTabName]").val() != "" ? $("[id*=hdfTabName]").val() : firstTab.attr("href").replace("#", "");
+            
+            if(tabName.indexOf('pnlTabCredit')>=0)
+            {
+            	paymentType='CC';
+				bill2payPaymentDetails.addCreditCardDetails();
+			}
+				
+			else
+			{
+								paymentType='ACH';
+				bill2payPaymentDetails.addBankDetails();	
+				}				
+			
+				
+	if(validateForm(paymentType))
+	{
+		var json=SerializeDbObject();		
+      redirect('PaymentConfirmlookup.html?ShowCart=Y&dbObject=' + json);	
+	}
+	
+},
+addCreditCardDetails:function(){
+	debugger;
+	var cc = {
+								"PaymentType": "CC",
+								"NameOnCard": $("#txtNameonCard").val(),
+								"CardNumber": $("#txtCreditCardNumber").val(),
+								"ExpDate":  $("#txtExpireDate").val().replace('/','|'),
+								"CVV":  $("#txtCCV").val(),
+								"Country":  $("#ddlCountry").val(),
+								"Zip":  $("#txtBillingZip").val(),								
+								
+						  };
+		billingDetails=cc;
+		UpdateDbObject();
+},
+populateCreditCardDetails:function(){
+	debugger;
+		
+	$("#txtNameonCard").val('');
+	$("#txtExpireDate").val('');
+	$("#ddlCountry").val('');
+	$("#txtBillingZip").val('');
+	if(dbObject.BillingDetails!=null){
+		
+	$("#txtNameonCard").val(dbObject.BillingDetails.NameOnCard);
+	$("#txtExpireDate").val(dbObject.BillingDetails.ExpDate.replace('_','/').replace('|','/'));
+	$("#ddlCountry").val(dbObject.BillingDetails.Country);
+	$("#txtBillingZip").val(dbObject.BillingDetails.Zip);
+	
+	}
+	
+},
+addBankDetails:function(){
+	var ach = {
+								"PaymentType": "ACH",
+								"NameonBank": $("#txtNameonBankAccount").val(),
+								"BankRoutingNumber": $("#txtBankRoutingNumber").val(),
+								"BankAccountNumber":  $("#txtBankAccountNumber").val(),
+															
+								
+						  };	
+						  billingDetails=ach;
+						  UpdateDbObject();
+},
+addMoreItems: function(){
+	debugger;
+	var json=SerializeDbObject();		
+    redirect('Homelookup.html?dbObject=' + json);	
+},
+populateSubtotal: function(){
+	var total=bill2payProducts.getCartTotalAmount();
+	$("#paysubTotalAmount").html(total);
+},
+populateOrderDetails: function(){
+	debugger;
+		var html = '';
+		var totalAmount=0;
+		var cellClass='';
+		 html += '<div class="table-responsive" style="padding:0;">';
+          html += '<table id="tblOrderInfo" class="table table-condensed table-no-border">';
+          html += '<div id="pnlItem1">';
+           for (var i = 0; i < dbObject.Products.length; i++) {
+           		var row=dbObject.Products[i];
+          		html += '<tr class="bg-primarydark">';
+          		html += '<td class="col-sm-6 text-uppercase"><label id="ItemName">'+(row.ProductName.toUpperCase().indexOf('TEST')>=0?row.ProductName.replace(' ',' &').replace('_',''):row.ProductName)+' </label> </td>';
+           		html += '<td style="text-align:right"><label id="ItemAmount">$' + parseFloat(bill2payAccountDetails.getItemAmount(i)).toFixed(2)+'</label> </td>';
+          		html +=  '</tr>';
+          		html +=  '<tr>';
+          		if(row.ACC1!=null){
+					html += '<td class="col-sm-6 text-uppercase">'+getKeyFromJson(row.ACC1)+'</td>';
+          			html += '<td style="text-align:right">'+getValueFromJson(row.ACC1)+'</td>';
+				}
+				html +=  '</tr>';
+           		if(row.ACC2!=null){
+					html += '<td class="col-sm-6 text-uppercase">'+getKeyFromJson(row.ACC2)+'</td>';
+          			html += '<td style="text-align:right">'+getValueFromJson(row.ACC2)+'</td>';
+				}
+				html +=  '</tr>';
+				if(row.ACC3!=null){
+					html += '<td class="col-sm-6 text-uppercase">'+getKeyFromJson(row.ACC3)+'</td>';
+          			html += '<td style="text-align:right">'+getValueFromJson(row.ACC3)+'</td>';
+				}
+           		html += '</tr>';
+           		
+           }
+ 		html +=           '</div>  ';                                                             
+ 		html +=           '</table>';
+       	html +=     '<div id="dvsubTotal">';
+        html +=    '<table id="tblOrderInfo" class="table table-condensed table-no-border">';
+       	html +=     '<div class="col-xs-12 bg-primarydark">';
+        html +=    '<tr class="bg-primarydark">';
+        html +=   '<td class="col-sm-6 text-uppercase"><label id="subTotal" >Subtotal </label> </td>';
+        html +=  '<td style="text-align:right"><label id="subTotalAmount"  >'+bill2payProducts.getCartTotalAmount().toString()+'</label> </td>';
+        html += '</tr>';                         
+        html +=  '</div>';
+        html +=  '</table>';
+        html += '</div>';
+ 		html +=       '</div>';
+        
+	  	$('#orderDetails').html('');
+	    $('#orderDetails').append(html); 
+},
+
+};
