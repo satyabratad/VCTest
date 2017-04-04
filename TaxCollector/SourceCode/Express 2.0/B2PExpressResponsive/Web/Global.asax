45258ff4ad9b60fbcd89b79630fdf7fb20ada654
JoyG <JoyG@Bill2PayTest>
2017-04-04 06:37:43 -0400

    ﻿ [git-vault-id] RS Software$@402/63745

--- TaxCollector/SourceCode/Express 2.0/B2PExpressResponsive/Web/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.vb" Inherits=".Global_asax" Language="vb" %>
--- TaxCollector/SourceCode/Express 2.0/B2PExpressResponsive/Web/UserControls/Footer.ascx.vb
+++
@@ -1,7 +0,0 @@
-﻿Namespace B2P.PaymentLanding.Express.Web
-
-    Public Class Footer : Inherits System.Web.UI.UserControl
-
-    End Class
-
-End Namespace
