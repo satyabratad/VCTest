68436ef786194817af38bd268e8f722873e78222
ArghyaB <ArghyaB@Bill2PayTest>
2017-04-07 04:38:15 -0400

    ﻿ [git-vault-id] RS Software$@429/63803

--- TaxCollector/SourceCode/Express2.1/B2P.Cart/PropertyAddress.vb
+++
@@ -1,7 +0,0 @@
-﻿Public Class PropertyAddress
-    Public Property Address1 As String
-    Public Property Address2 As String
-    Public Property City As String
-    Public Property State As String
-    Public Property Zip As String
-End Class
