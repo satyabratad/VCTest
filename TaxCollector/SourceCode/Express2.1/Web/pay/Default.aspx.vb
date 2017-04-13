2df38fb99c5c91192c14f8e41afb205d4300e664
JoyG <JoyG@Bill2PayTest>
2017-04-13 09:30:28 -0400

    ﻿visibility for lookup [git-vault-id] RS Software$@515/63950

--- TaxCollector/SourceCode/Express2.1/Web/pay/Default.aspx.vb
+++ TaxCollector/SourceCode/Express2.1/Web/pay/Default.aspx.vb
@@ -379,11 +379,16 @@ Namespace B2P.PaymentLanding.Express.Web
                     grdLookup.DataSource = Nothing
                     BuildForm()
                 End If
-                If z.CollectAddress Then
-                    ctlPropertyAddress.AddressVisible = True
-                Else
-                    ctlPropertyAddress.AddressVisible = False
+                If BLL.SessionManager.ClientType = B2P.Cart.EClientType.NonLookup Then
+                    If z.CollectAddress Then
+                        ctlPropertyAddress.AddressVisible = True
+                    Else
+                        ctlPropertyAddress.AddressVisible = False
+                    End If
+                ElseIf BLL.SessionManager.ClientType = B2P.Cart.EClientType.Lookup Then
+                    ctlPropertyAddress.Visible = False
                 End If
+
 
                 If BLL.SessionManager.ManageCart.ShowCart = True Then
                     pnlCart.Visible = True
