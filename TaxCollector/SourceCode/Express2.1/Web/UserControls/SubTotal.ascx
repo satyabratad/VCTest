b31f2b047e98d428a396c5aeccd6c6a8004b97d9
JoyG <JoyG@Bill2PayTest>
2017-04-21 02:22:52 -0400

    ﻿added [git-vault-id] RS Software$@542/64028

---
+++ TaxCollector/SourceCode/Express2.1/Web/UserControls/SubTotal.ascx
@@ -0,0 +1,12 @@
+﻿<%@ Control Language="vb" AutoEventWireup="false" CodeBehind="SubTotal.ascx.vb" Inherits=".SubTotal" %>
+<div>
+<table class="table" style="width: 100%;" id="tblsubTotal">
+    <tr >
+        <td class="table-header">Sub Total</td>
+        <td class="table-header" style="float:right ">
+            
+            <asp:Label ID="lblSubTotal" runat="server" Text=""></asp:Label>
+        </td>
+    </tr>
+</table>
+</div>
---
+++ TaxCollector/SourceCode/Express2.1/Web/UserControls/SubTotal.ascx.vb
@@ -0,0 +1,21 @@
+﻿Imports B2P.PaymentLanding.Express
+Imports B2P.PaymentLanding.Express.Web
+Public Class SubTotal
+    Inherits System.Web.UI.UserControl
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        If Page.IsPostBack = False Then
+            lblSubTotal.Text = CalculateSubTotal()
+        End If
+
+    End Sub
+    Protected Function CalculateSubTotal() As String
+        Dim total As Double = 0
+        If Not BLL.SessionManager.ManageCart.Cart Is Nothing Then
+            For Each item As B2P.Cart.Cart In BLL.SessionManager.ManageCart.Cart
+                total += item.Amount
+            Next
+        End If
+        Return String.Format("{0:C}", total)
+    End Function
+End Class
