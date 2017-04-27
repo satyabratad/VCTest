022f57e26f0bded2dd9b8ea6ca02e1bed9d45896
JoyG <JoyG@Bill2PayTest>
2017-04-27 07:18:18 -0400

    ﻿comment added [git-vault-id] RS Software$@621/64194

--- TaxCollector/SourceCode/Express2.1/Web/UserControls/OrderDetails.ascx.vb
+++ TaxCollector/SourceCode/Express2.1/Web/UserControls/OrderDetails.ascx.vb
@@ -1,6 +1,9 @@
 ﻿Imports B2P.PaymentLanding.Express
 Imports B2P.PaymentLanding.Express.BLL
 Imports B2P.PaymentLanding.Express.Web
+''' <summary>
+''' This will display the order details in a list grid format
+''' </summary>
 Public Class OrderDetails
     Inherits System.Web.UI.UserControl
 
@@ -11,7 +14,10 @@ Public Class OrderDetails
         End If
     End Sub
 
-
+    ''' <summary>
+    ''' This will populate the Order detailsby binding the items in grid
+    ''' </summary>
+    ''' <returns></returns>
     Protected Function PopulateOrderGrid()
 
         rptOrder.DataSource = BLL.SessionManager.ManageCart.Cart
@@ -22,9 +28,16 @@ Public Class OrderDetails
 
 
     End Sub
+    ''' <summary>
+    ''' This will return display formated amount
+    ''' </summary>
+    ''' <returns></returns>
     Protected Function FormatAmount(Amount As Double) As String
         Return String.Format("{0:C}", Amount)
     End Function
+    ''' <summary>
+    ''' This will populate the Order detailsby binding the account details of Item/service in grid
+    ''' </summary>
     Protected Sub rptOrder_ItemDataBound(sender As Object, e As RepeaterItemEventArgs) Handles rptOrder.ItemDataBound
         Dim item As RepeaterItem = e.Item
         Dim AccRepeater As Repeater = DirectCast(e.Item.FindControl("rptOrderAccount"), Repeater)
