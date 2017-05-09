344ef3db83fbac6ade261ad2c70a6692a3527381
BAyan <BAyan@Bill2PayTest>
2017-05-09 07:23:29 -0400

    ﻿ [git-vault-id] RS Software$@695/64327

---
+++ Deployment 1.1/Application-05-09-2017/Scripts/jscsv.js
@@ -0,0 +1,62 @@
+﻿jQuery.fn.tableToCSV = function (name) {
+    var clean_text = function (text) {
+        text = text.replace(/ /g, '');
+        text = text.replace(/\n/g, '');
+        text = text.replace(/"/g, '""');
+        
+        return '"' + text + '"';
+    };
+
+    $(this).each(function () {
+        var table = $(this);
+        var caption = $(this).find('caption').text();
+        var title = [];
+        var rows = [];
+
+        $(this).find('tr').each(function () {
+            var data = [];
+            $(this).find('th').each(function () {
+                var text = clean_text($(this).text());
+                title.push(text);
+            });
+            $(this).find('td').each(function () {
+                var text = clean_text($(this).text());
+                data.push(text);
+            });
+            data = data.join(",");
+
+            rows.push(data);
+        });
+        title = title.join(",");
+        rows = rows.join("\n");
+        var d = new Date();
+        var curr_date = d.getDate();
+        var curr_month = d.getMonth() + 1; //Months are zero based
+        var curr_year = d.getFullYear();
+
+        var ts = curr_month + '_' + curr_date + "_" + curr_year;
+        name = name + ts;
+        var csv = title + rows;
+        var blob = new Blob([csv], { type: "text/csv" });
+        // Determine which approach to take for the download
+        if (navigator.msSaveOrOpenBlob) {
+            // Works for Internet Explorer and Microsoft Edge
+            navigator.msSaveOrOpenBlob(blob, name+".csv");
+
+        } else {
+
+            // Attempt to use an alternative method
+            var anchor = document.body.appendChild(
+              document.createElement("a")
+            );
+            // If the [download] attribute is supported, try to use it
+            if ("download" in anchor) {
+                anchor.download = name+".csv";
+                anchor.href = URL.createObjectURL(blob);
+                anchor.click();
+            }
+
+        }
+    });
+
+};
